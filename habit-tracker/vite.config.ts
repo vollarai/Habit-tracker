ed705036b00c3291c5e6086c85b2671ae74a5895
Volha Rai <vollarai@gmail.com>
2025-06-25 18:05:04 +0200

    fix

--- habit-tracker/vite.config.ts
+++ habit-tracker/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Habit-tracker/',
+  base: '/Habit-tracker/habit-tracker/',
   plugins: [react()],
 })
